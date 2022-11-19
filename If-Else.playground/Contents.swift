95a1cb4b2094dd4f2873f02727d2447dcbfd2c12
Anirudh Pratap Singh <36331702+flutterninja9@users.noreply.github.com>
2022-11-19 13:00:28 +0530

    added if-else

---
+++ If-Else.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+
+let myName = "Anirudh"
+let myAge = 24
+let yourName = "Jitesh"
+let youAge = 28
+
+if yourName == "Jitesh" {
+    print("Yay! guessed it right!")
+} else {
+    print("Oops! \(yourName) guessed it wrong")
+}
+
